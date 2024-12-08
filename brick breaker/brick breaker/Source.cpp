e284d353b1a919e316ce8041999e238fa1d8bbdd
mohamed alaa <m-aa2001@hotmail.com>
2024-12-08 03:52:17 +0200

    added wall

--- brick breaker/brick breaker/Source.cpp
+++ brick breaker/brick breaker/Source.cpp
@@ -25,7 +25,7 @@ struct vertex
 GLuint InitShader(const char* vertex_shader_file_name, const char* fragment_shader_file_name);
 
 const GLint WIDTH = 600, HEIGHT = 600;
-GLuint VBO_Ball, VBO_Paddle, IBO;
+GLuint VBO_Ball, VBO_Paddle, VBO_Wall, IBO;
 GLuint BasiceprogramId, phongProgramId, smoothProgramId;
 float mousposX;
 // transformation
@@ -33,13 +33,66 @@ GLuint modelMatLoc, viewMatLoc, projMatLoc;
 
 bool firstStart = true;
 
+int vertices_Indeces[] = {
+	//front
+	0,
+	1,
+	2,
+
+	0,
+	2,
+	3,
+	//Right
+	3,
+	2,
+	5,
+
+	3,
+	5,
+	4,
+	//Back
+	4,
+	5,
+	6,
+
+	4,
+	6,
+	7,
+	//Left
+	7,
+	6,
+	1,
+
+	7,
+	1,
+	0,
+	//Top
+	7,
+	0,
+	3,
+
+	7,
+	3,
+	4,
+	//Bottom
+	2,
+	1,
+	6,
+
+	2,
+	6,
+	5
+};
+
 #pragma region Paddle
 
-const float paddleW = 1, paddleH = 0.2, paddleD = 0.2;
+
 vertex paddleVertices[];
 
 void CreatePaddle(float posX)
 {
+	const float paddleW = 1, paddleH = 0.2, paddleD = 0.2;
+
 	vec3 center(posX,0,0);
 	vertex paddleVertices[] = 
 	{
@@ -52,57 +105,6 @@ void CreatePaddle(float posX)
 		{vec3(center.x - paddleW / 2, center.y - paddleH / 2, center.z - paddleD / 2),vec3(1,1,1)},
 		{vec3(center.x - paddleW / 2, center.y + paddleH / 2, center.z - paddleD / 2),vec3(0,0,0)}
 	};
-	int vertices_Indeces[] = {
-		//front
-		0,
-		1,
-		2,
-
-		0,
-		2,
-		3,
-		//Right
-		3,
-		2,
-		5,
-
-		3,
-		5,
-		4,
-		//Back
-		4,
-		5,
-		6,
-
-		4,
-		6,
-		7,
-		//Left
-		7,
-		6,
-		1,
-
-		7,
-		1,
-		0,
-		//Top
-		7,
-		0,
-		3,
-
-		7,
-		3,
-		4,
-		//Bottom
-		2,
-		1,
-		6,
-
-		2,
-		6,
-		5
-	};
-
 	// create VBO
 	glGenBuffers(1, &VBO_Paddle);
 
@@ -206,6 +208,68 @@ void BindSphere()
 	glEnableVertexAttribArray(1);
 }
 #pragma endregion 
+
+#pragma region Wall
+
+
+vertex wallVertices[];
+
+void CreateWall()
+{
+	const float WallW = 6, WallH = 6.5, WallD = 0.2;
+	vec3 center(0, 0, 0);
+	vec3 color(0, 0.5, 0.5);
+	vertex wallVertices[] =
+	{
+		{vec3(center.x - WallW / 2, center.y + WallH / 2, center.z + WallD / 2),color},
+		{vec3(center.x - WallW / 2, center.y - WallH / 2, center.z + WallD / 2),color},
+		{vec3(center.x + WallW / 2, center.y - WallH / 2, center.z + WallD / 2),color},
+		{vec3(center.x + WallW / 2, center.y + WallH / 2, center.z + WallD / 2),color},
+		{vec3(center.x + WallW / 2, center.y + WallH / 2, center.z - WallD / 2),color},
+		{vec3(center.x + WallW / 2, center.y - WallH / 2, center.z - WallD / 2),color},
+		{vec3(center.x - WallW / 2, center.y - WallH / 2, center.z - WallD / 2),color},
+		{vec3(center.x - WallW / 2, center.y + WallH / 2, center.z - WallD / 2),color}
+	};
+
+	// create VBO
+	glGenBuffers(1, &VBO_Wall);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_Wall);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(wallVertices), wallVertices, GL_DYNAMIC_DRAW);
+
+	// Index Buffer
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices_Indeces), vertices_Indeces, GL_DYNAMIC_DRAW);
+
+	// shader
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(vertex), 0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(vertex), (char*)(sizeof(vec3)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, false, sizeof(vertex), (char*)(2 * sizeof(vec3)));
+	glEnableVertexAttribArray(2);
+
+}
+
+void BindWall()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_Wall);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(vertex), 0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(vertex), (char*)(sizeof(vec3)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, false, sizeof(vertex), (char*)(2 * sizeof(vec3)));
+	glEnableVertexAttribArray(2);
+}
+
+#pragma endregion 
 void CompileShader(const char* vertex_shader_file_name, const char* fragment_shader_file_namering, GLuint& programId)
 {
 	programId = InitShader(vertex_shader_file_name, fragment_shader_file_namering);
@@ -218,7 +282,7 @@ void UseShader(GLuint InProgramID)
 	viewMatLoc = glGetUniformLocation(InProgramID, "viewMat");
 	projMatLoc = glGetUniformLocation(InProgramID, "projMat");
 
-	glm::mat4 viewMat = glm::lookAt(glm::vec3(0, 2, 5), glm::vec3(0, 1, 0), glm::vec3(0, 1, 0));
+	glm::mat4 viewMat = glm::lookAt(glm::vec3(0, 0, 5), glm::vec3(0, 1, 0), glm::vec3(0, 1, 0));
 	glUniformMatrix4fv(viewMatLoc, 1, GL_FALSE, glm::value_ptr(viewMat));
 
 	glm::mat4 projMat = glm::perspectiveFov(60.0f, (float)WIDTH, (float)HEIGHT, 0.1f, 100.0f);
@@ -249,10 +313,11 @@ int Init()
 	CompileShader("VSPhong.glsl", "FSPhong.glsl", phongProgramId);
 	CompileShader("VSSmooth.glsl", "FSSmooth.glsl", smoothProgramId);
 
+	CreateWall();
 	CreatePaddle(0);
 	CreateSphere(4);
 
-	glClearColor(0, 0.5, 0.5, 1);
+	glClearColor(0, 0.7, 0.7, 1);
 	glEnable(GL_DEPTH_TEST);
 
 	return 0;
@@ -270,15 +335,25 @@ void Render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-	BindPaddle();
 	UseShader(BasiceprogramId);
-	// draw cube
-	mat4 ModelMat = glm::translate(glm::vec3(mousposX, -1, 0)) *
+	BindWall();
+	// draw Wall
+	mat4 ModelMat = glm::translate(glm::vec3(0, 1.5, -1)) *
 		glm::rotate(0.0f, glm::vec3(1, 0, 0)) *
 		glm::scale(glm::vec3(1, 1, 1));
 	glUniformMatrix4fv(modelMatLoc, 1, GL_FALSE, glm::value_ptr(ModelMat));
 
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, NULL);
+
+	BindPaddle();
+	// draw Paddle
+	ModelMat = glm::translate(glm::vec3(mousposX, -1, 0)) *
+		glm::rotate(0.0f, glm::vec3(1, 0, 0)) *
+		glm::scale(glm::vec3(1, 1, 1));
+	glUniformMatrix4fv(modelMatLoc, 1, GL_FALSE, glm::value_ptr(ModelMat));
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, NULL);
+
 	UseShader(smoothProgramId);
 	BindSphere();
 	// draw Ball
@@ -288,8 +363,13 @@ void Render()
 			glm::rotate(theta * 180 / 3.14f, glm::vec3(1, 1, 1)) *
 			glm::scale(glm::vec3(0.1, 0.1, 0.1));
 	}
+	else
+	{
+		ModelMat = glm::translate(glm::vec3(0, -0.8, 0)) *
+			glm::rotate(theta * 180 / 3.14f, glm::vec3(1, 1, 1)) *
+			glm::scale(glm::vec3(0.1, 0.1, 0.1));
+	}
 	glUniformMatrix4fv(modelMatLoc, 1, GL_FALSE, glm::value_ptr(ModelMat));
-
 	glDrawArrays(GL_TRIANGLES, 0, sphere_vertices.size());
 
 }
@@ -314,20 +394,13 @@ int main()
 				window.close();
 				break;
 			}
-			case sf::Event::KeyPressed:
+			case sf::Event::MouseButtonPressed:
 			{
-				/*if (event.key.code == sf::Keyboard::Num1)
+				if (event.key.code == sf::Mouse::Left)
 				{
-					Current_DrawingMode = DrawingMode::Points;
+					firstStart = false;
+					printf("ball shoot");
 				}
-				if (event.key.code == sf::Keyboard::Num2)
-				{
-					Current_DrawingMode = DrawingMode::Lines;
-				}
-				if (event.key.code == sf::Keyboard::Num3)
-				{
-					Current_DrawingMode = DrawingMode::FilledTriangle;
-				}*/
 				break;
 			}
 			}
